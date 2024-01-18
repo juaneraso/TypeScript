32d13f890f94a2c8e6864ac4f4e9bd0f3883efe7
Juan <juangui9508@hotmail.com>
2024-01-18 09:46:21 -0500

    innecesary files

--- numeros.js
+++
@@ -1,7 +0,0 @@
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// Ejemplo: obtener un número aleatorio entre 1 y 10
-const randomNumber = getRandomNumber(1, 45);
-console.log(randomNumber);
